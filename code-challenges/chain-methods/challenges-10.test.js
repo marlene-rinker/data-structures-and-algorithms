336f1be7467dcb5a12dcf9ac2857346771ac9991
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-05-15 17:15:39 -0700

    finished question 4

--- code-challenges/chain-methods/challenges-10.test.js
+++ code-challenges/chain-methods/challenges-10.test.js
@@ -85,8 +85,14 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  return input.map(result => {
+    return result.filter(val => val % 5 === 0 && typeof val === 'number').map(num => {
+      return Math.pow(2, num);
+    })
+  })
 };
+// one liner way for reference
+// const divisibleByFiveTwoToThePower = (input) => input.map(result => result.filter(val => val % 5 === 0 && typeof val === 'number').map(num => Math.pow(2,num)));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 - Stretch Goal
@@ -234,7 +240,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return numbers divisible by five, then raise two to the power of the resulting numbers', () => {
     expect(divisibleByFiveTwoToThePower([[10, 20, 5, 4], [5, 6, 7, 9], [1, 10, 3]])).toStrictEqual([[1024, 1048576, 32], [32], [1024]]);
   });
