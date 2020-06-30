62a10a6c24046bf7ea9fb5689467cade08590e90
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-06-30 14:01:04 -0700

    removed unnecessary code

--- data-structures/tree/tree.js
+++ data-structures/tree/tree.js
@@ -203,17 +203,7 @@ class BinarySearchTree {
   }
 }
 
-let tree = new BinaryTree();
-tree.root = new Node(2);
-tree.root.left = new Node(7);
-tree.root.right = new Node(5);
-tree.root.left.left = new Node(2);
-tree.root.left.right = new Node(6);
-tree.root.left.right.left = new Node(5);
-tree.root.left.right.right = new Node(11);
-tree.root.right.right = new Node(9);
-tree.root.right.right.left = new Node(4);
-console.log(tree.traverseBreadthFirst());
+
 
 module.exports = {
   Node,
