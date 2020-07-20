31fef8f6eefdfe0f254e9646ef2c0388dfc2918d
marlene-rinker <marlene.rinker@prtechnologies.com>
2020-07-20 15:09:04 -0700

    updated readme and removed unnecessary comments

--- challenges/repeatedWord/repeated-word.js
+++ challenges/repeatedWord/repeated-word.js
@@ -3,11 +3,7 @@
 const HashTable = require('../../data-structures/hashtable/hashtable.js');
 
 function findFirstRepeatedWord(str) {
-//split string to get array of words
-// for each word, using the word as a key,
-//if the hashtable contains the key, compare the values, if not equal, then add the value to the table
-// if the values match, return the value as the first repeated one
-// if the hashtable doesn't contain the key, add the value
+
 
 let newString = str.replace(/[^a-zA-Z ]/g, '');//get rid of anything that's not a word, but leave spaces
 
@@ -39,12 +35,3 @@ return repeatedWord;
 
 module.exports = findFirstRepeatedWord;
 
-// let myString = 'Once upon a time, there was a brave princess who...'
-// let myString2 = 'It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only...'
-// let myString3 = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...'
-// let myString4 = 'It is a beautiful day...';
-
-// console.log(findFirstRepeatedWord(myString));
-// console.log(findFirstRepeatedWord(myString2));
-// console.log(findFirstRepeatedWord(myString3));
-// console.log(findFirstRepeatedWord(myString4));
--- challenges/repeatedWord/repeated-word.md
+++ challenges/repeatedWord/repeated-word.md
@@ -8,9 +8,7 @@ Find the first repeated word in a "book"
 
 
 ## Approach & Efficiency
-Created a HashTable class with a constructor and four methods. Created tests to verify the functionality. (Copied in Node and LinkedList classes to use with the HashTable class).
-
-In the HashTable class, `hash` takes in a key and uses ascii values and math to determine the index. This has an efficiency of O(n). `add` takes in a key and value and uses the hash method to determine where to put the key/value and put them in the table. This usually has an efficiency of O(1). `get` takes in a key and uses the hash method to find its index so it can get the value. This usually has an efficiency of O(1). `contains` takes in a key and uses the hash method to find its index and then checks to see if its in the table or not and returns a boolean. This usually has an efficiency of O(1).
+Created a function that takes in a string. It removes non-letters from the string and puts each word into an array. It creates a hashtable and checks to see if the word is in the table. If not, it adds it. If so, it returns the word. This has a `O(n)` efficiency because it may have to go through every word in the string.
 
 
 ## Whiteboard
